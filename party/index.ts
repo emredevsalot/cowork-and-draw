c45aa71800e6fbca0fecc9cb282b046d791466f3
Emre Ergül <emredevsalot@gmail.com>
2023-10-26 16:57:26 +0300

    set up PartyKit server

--- party/index.ts
+++ party/index.ts
@@ -1,30 +1,25 @@
 import type * as Party from "partykit/server";
+import type { Poll } from "@/app/types";
 
 export default class Server implements Party.Server {
   constructor(readonly party: Party.Party) {}
 
-  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.party.id}
-  url: ${new URL(ctx.request.url).pathname}`
-    );
+  poll: Poll | undefined;
 
-    // let's send a message to the connection
-    conn.send("hello from server");
-  }
+  async onRequest(req: Party.Request) {
+    if (req.method === "POST") {
+      const poll = (await req.json()) as Poll;
+      this.poll = { ...poll, votes: poll.options.map(() => 0) };
+    }
 
-  onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.party.broadcast(
-      `${sender.id}: ${message}`,
-      // ...except for the connection it came from
-      [sender.id]
-    );
+    if (this.poll) {
+      return new Response(JSON.stringify(this.poll), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    return new Response("Not found", { status: 404 });
   }
 }
 
